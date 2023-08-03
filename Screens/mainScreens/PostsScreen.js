ebab55a79af616680a104b2dfa13d3105c91fd47
Tetiana2023 <drofaj18@gmail.com>
2023-08-03 17:07:50 +0300

    made post page

--- Screens/mainScreens/PostsScreen.js
+++ Screens/mainScreens/PostsScreen.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { View, Text,  StyleSheet} from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export const PostsScreen = ()=> {
-    return (
-        <View style={styles.container}>
-            <Text > PostsScreen</Text>
-        </View>
-    )
-}
+import ExitBtn from "../../assets/img/log-out.png";
+
+export const PostsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}> Публікації</Text>
+        <TouchableOpacity style={styles.exitBtn}>
+          onPress={}
+          <Image 
+           style={styles.exitIcon}
+          source={ExitBtn} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-  })
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  exitIcon:{
+    width: 24,
+    height: 24,
+  }
+});
--- router.js
+++ router.js
@@ -5,11 +5,15 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const AuthStack = createNativeStackNavigator();
 const MainTab = createBottomTabNavigator();
 
+import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+
 import { LoginScreen } from "./Screens/auth/LoginScreen";
 import { RegistrationScreen } from "./Screens/auth/RegistrationScreen";
 import { PostsScreen } from "./Screens/mainScreens/PostsScreen";
 import { CreatePostsScreen } from "./Screens/mainScreens/CreatePostsScreen";
 import { ProfileScreen } from "./Screens/mainScreens/ProfileScreen";
+import { View } from "react-native-web";
 
 export const useRoute = (isAuth) => {
   if (!isAuth) {
@@ -29,22 +33,78 @@ export const useRoute = (isAuth) => {
     );
   }
   return (
-    <MainTab.Navigator>
+    <MainTab.Navigator
+      screenOptions={{
+        tabBarShowLable: false,
+        tabBarStyle:{
+          display:"flex",
+          justifyContent: "center", 
+          height:83,
+  }
+      }}
+    >
       <MainTab.Screen
         name="Home"
         component={PostsScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            if (!focused) {
+              return <AntDesign name="appstore-o" size={24} color="rgba(33, 33, 33, 0.8)" />;
+            }
+            return (
+              <View style={styles.container}>
+                <AntDesign name="appstore-o" size={24} color="#FFF" />;
+              </View>
+            );
+          },
+        }}
       />
       <MainTab.Screen
         name="CreatePostsScreen"
         component={CreatePostsScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            if (!focused) {
+              return <Feather name="plus" size={24} color="rgba(33, 33, 33, 0.8)" />;
+            }
+            return (
+              <View style={styles.container}>
+                <Feather name="plus" size={24} color="#fff" />;
+              </View>
+            );
+          },
+        }}
       />
       <MainTab.Screen
         name="ProfileScreen"
         component={ProfileScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            if (!focused) {
+              return <Feather name="user" size={24} color="rgba(33, 33, 33, 0.8)" />;
+            }
+            return (
+              <View style={styles.container}>
+                <Feather name="user" size={24} color="#fff" />;
+              </View>
+            );
+          },
+        }}
       />
     </MainTab.Navigator>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    borderRadius:100,
+    backgroundColor:"#FF6C00",
+    width: 70,
+    height: 40,
+  },
+})
