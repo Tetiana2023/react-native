71b592adc4505feeeff34e7c07e707cadd49b668
Tetiana2023 <drofaj18@gmail.com>
2023-08-25 14:40:28 +0300

    prof screen

--- Screens/mainScreens/ProfileScreen.js
+++ Screens/mainScreens/ProfileScreen.js
@@ -1,27 +1,27 @@
 import React from "react";
-import { View, Text,  StyleSheet, TouchableOpacity} from "react-native";
-import {useDispatch} from 'react-redux';
-import {authSignOutUser} from '../../redux1/auth/authOperations';
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useDispatch } from "react-redux";
+import { authSignOutUser } from "../../redux1/auth/authOperations";
 
-export const ProfileScreen = ()=> {
-    const dispatch = useDispatch();
-const signOut = ( )=> {
-dispatch(authSignOutUser())
-}
+export const ProfileScreen = () => {
+  const dispatch = useDispatch();
+  const signOut = () => {
+    dispatch(authSignOutUser());
+  };
 
-    return (
-     
-        <View style={styles.container}>
-            <Text> ProfileScreen</Text>
-           < TouchableOpacity onPress={signOut}> log out</TouchableOpacity>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text> ProfileScreen</Text>
+      <TouchableOpacity onPress={signOut}>
+        <Text>log out</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    }
-
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
